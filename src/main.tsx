5d68f58efcccea3ad9fe12bb018b145f16e1bd10
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-02-10 21:40:09 -0500

    add routing to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Index from "./Index.tsx";
+import About from "./pages/About.tsx";
+import Cooks from "./pages/cooks.tsx";
+import Projects from "./pages/Projects.tsx";
 import "./index.css";
+import { Route, Routes, Link, BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Index />
+    <BrowserRouter>
+      <main>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/cooks" element={<Cooks />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+    {/* <About /> */}
   </React.StrictMode>
 );
