6542ca95d1827c1d8bb71472f1f1de63427aa371
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-02-26 12:46:49 -0500

    start cooks page

---
+++ src/pages/Cooks.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Cooks() {
+  return (
+    <>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
+        rel="stylesheet"
+      />
+      <div className="mx-[0%] md:mx-[25%] lg:mx-[25%] min-h-screen flex flex-col"></div>
+    </>
+  );
+}
+
+export default Cooks;
