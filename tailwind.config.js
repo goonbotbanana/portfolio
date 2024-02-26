8630795c5b362f5ed13f9135d5a64341c6319d78
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-02-26 01:27:59 -0500

    define semibold and light-blue

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: { Roboto: ["Roboto", "sans-serif"] },
+      fontWeight: {
+        semibold: 500,
+      },
       colors: {
         "dark-blue": "#77A1BD",
+        "light-blue": "#A3C6D9",
       },
     },
   },
