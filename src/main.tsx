77d247f07da8ba944d92b2183458b8f83f29b71b
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-02-26 04:45:12 -0500

    remove cooks and resume

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import About from "./pages/About.tsx";
-import Cooks from "./pages/Cooks.tsx";
 import Work from "./pages/Work.tsx";
-import Resume from "./pages/Resume.tsx";
 import "./index.css";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
@@ -13,9 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <main>
         <Routes>
           <Route path="/" element={<About />} />
-          <Route path="/cooks" element={<Cooks />} />
           <Route path="/work" element={<Work />} />
-          <Route path="/resume" element={<Resume />} />
         </Routes>
       </main>
     </BrowserRouter>
