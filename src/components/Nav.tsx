6e32d4530047eed013165f2a6ba4c9a817848102
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-02-29 15:01:05 -0500

    remove memo import for build bruh

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import { memo } from "react";
+// import { memo } from "react";
 import { motion } from "framer-motion";
 
 // const Image = memo(function Image({ kanyegoat }: { kanyegoat: string }) {
