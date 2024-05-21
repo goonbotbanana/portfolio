ecec1e1c7d6ce454b0156bc7f5ca19aa0c99d40d
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-05-21 19:50:42 -0400

    replace dropdown with sheet component, update styling

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 import { motion } from "framer-motion";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
@@ -17,119 +11,110 @@ function Nav() {
   return (
     <>
       <div className="text-left flex flex-row sm:my-4">
-        <NavigationMenu className="sm:hidden">
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger className="p-0">
-                <HamburgerMenuIcon width="25" height="25" />
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="flex flex-col gap-3 p-3">
-                  <a
-                    className="transition-all duration-300 rounded-lg hover:text-slate-500"
-                    href="#/"
-                  >
-                    <NavigationMenuLink>
-                      <Link
-                        to="/"
-                        className={
-                          location.pathname == "/"
-                            ? "transition-all duration-300 text-slate-500 pointer-events-none"
-                            : "no-underline"
-                        }
-                      >
-                        About
-                      </Link>
-                    </NavigationMenuLink>
-                  </a>
-                  <a
-                    className=" transition-all duration-300 hover:text-slate-500"
-                    href="#/projects"
-                  >
-                    {" "}
-                    <Link
-                      to="/projects"
-                      className={
-                        location.pathname == "/projects"
-                          ? "transition-all duration-300 text-slate-500  pointer-events-none"
-                          : "no-underline"
-                      }
-                    >
-                      Projects
-                    </Link>
-                  </a>
-                  <a
-                    className=" transition-all duration-300 hover:text-slate-500"
-                    href="#/work"
-                  >
-                    {" "}
-                    <Link
-                      to="/work"
-                      className={
-                        location.pathname == "/work"
-                          ? "transition-all duration-300 text-slate-500  pointer-events-none"
-                          : "no-underline"
-                      }
-                    >
-                      Work
-                    </Link>
-                  </a>
-                  <a
-                    className="items-center transition-all duration-300 hover:text-slate-500"
-                    href="#/resume"
-                  >
-                    {" "}
-                    <Link
-                      to="https://drive.google.com/file/d/12YdcXV5d95G_JmFGbJBBZ5NfJ1g-Ti7p/view?usp=sharing"
-                      target="_blank"
-                    >
-                      Resume
-                    </Link>
-                  </a>
-                  <a
-                    className="items-center transition-all duration-300 hover:text-slate-500 flex flex-row gap-2"
-                    href="#/github"
-                  >
-                    {" "}
-                    <Link to="https://github.com/goonbotbanana" target="_blank">
-                      Github
-                    </Link>
-                    <svg
-                      className="fill-black w-6 h-auto"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="25"
-                      height="25"
-                      viewBox="0 0 25 25"
-                    >
-                      <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path>
-                    </svg>
-                  </a>
-                  <a
-                    className="items-center transition-all duration-300 hover:text-slate-500 flex flex-row gap-2"
-                    href="#/linkedin"
-                  >
-                    {" "}
-                    <Link
-                      to="https://www.linkedin.com/in/pharit/"
-                      target="_blank"
-                    >
-                      LinkedIn
-                    </Link>
-                    <svg
-                      className="fill-black w-6 h-auto rounded-lg"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"></path>
-                    </svg>
-                  </a>
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+        <Sheet>
+          <SheetTrigger className="p-0 sm:hidden">
+            <HamburgerMenuIcon width="25" height="25" />
+          </SheetTrigger>
+          <SheetContent side="left" className="w-1/2">
+            <ul className="flex flex-col gap-3 p-3">
+              <a
+                className="transition-all duration-300 rounded-lg hover:text-slate-500"
+                href="#/"
+              >
+                <Link
+                  to="/"
+                  className={
+                    location.pathname == "/"
+                      ? "transition-all duration-300 text-slate-500 pointer-events-none underline"
+                      : "no-underline"
+                  }
+                >
+                  About
+                </Link>
+              </a>
+              <a
+                className=" transition-all duration-300 hover:text-slate-500"
+                href="#/projects"
+              >
+                {" "}
+                <Link
+                  to="/projects"
+                  className={
+                    location.pathname == "/projects"
+                      ? "transition-all duration-300 text-slate-500  pointer-events-none underline"
+                      : "no-underline"
+                  }
+                >
+                  Projects
+                </Link>
+              </a>
+              <a
+                className=" transition-all duration-300 hover:text-slate-500"
+                href="#/work"
+              >
+                {" "}
+                <Link
+                  to="/work"
+                  className={
+                    location.pathname == "/work"
+                      ? "transition-all duration-300 text-slate-500  pointer-events-none underline"
+                      : "no-underline"
+                  }
+                >
+                  Work
+                </Link>
+              </a>
+              <a
+                className="items-center transition-all duration-300 hover:text-slate-500"
+                href="#/resume"
+              >
+                {" "}
+                <Link
+                  to="https://drive.google.com/file/d/12YdcXV5d95G_JmFGbJBBZ5NfJ1g-Ti7p/view?usp=sharing"
+                  target="_blank"
+                >
+                  Resume
+                </Link>
+              </a>
+              <a
+                className="items-center transition-all duration-300 hover:text-slate-500 flex flex-row gap-2"
+                href="#/github"
+              >
+                {" "}
+                <Link to="https://github.com/goonbotbanana" target="_blank">
+                  Github
+                </Link>
+                <svg
+                  className="fill-black w-6 h-auto"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="25"
+                  viewBox="0 0 25 25"
+                >
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path>
+                </svg>
+              </a>
+              <a
+                className="items-center transition-all duration-300 hover:text-slate-500 flex flex-row gap-2"
+                href="#/linkedin"
+              >
+                {" "}
+                <Link to="https://www.linkedin.com/in/pharit/" target="_blank">
+                  LinkedIn
+                </Link>
+                <svg
+                  className="fill-black w-6 h-auto rounded-lg"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"></path>
+                </svg>
+              </a>
+            </ul>
+          </SheetContent>
+        </Sheet>
 
         <h1 className="text-xl font-semibold hidden sm:block">
           <a
